3a8998620f6089ed4457e6ae0d06b56b229d20c0
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-19 09:47:01 +0100

    Fixed tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -14,16 +14,16 @@ from main import *
 class helper : 
      def check_random_walk( start, n, p ) :
          fpos = random_walk( start, n, p ) 
-         return (n+fpos) / 2
+         return (n+fpos-start) / 2
 
 class UnitTests(unittest.TestCase) :
     def test_random_walk(self) : 
         inputs, variables = [], []
-        for s in range(-3,3) : 
-            for n in range(1,8) :
-                for i in range(1,9) :
-                    p = i*0.1
-                    inputs.append((s,n,i*0.1,))
+        for s in range(-2,2) : 
+            for n in range(1,5) :
+                for i in range(1,5) :
+                    p = i*0.2
+                    inputs.append((s,n,p,))
                     myvar = randomvar( n*p, variance=n*p*(1-p), vmin=0, vmax=n, isinteger=True )
                     variables.append( myvar )
         assert( check_func('check_random_walk',inputs, variables, modname=helper ) )
