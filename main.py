3c870e0a0a38e19350fc446d6b14023fe21973a5
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-19 09:47:40 +0100

    Added answer

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import numpy as np
+
+def random_walk( startpoint, nsteps, p ) :
+    for i in range(nsteps) : 
+        if np.random.uniform(0,1)<p : startpoint = startpoint + 1 
+        else : startpoint = startpoint - 1
+    return startpoint   
